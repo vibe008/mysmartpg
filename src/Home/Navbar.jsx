6e52f7f82431849516edf3c60cfd4089fe3e5125
Vivek Bunker <vivekbunker65@gmail.com>
2025-08-23 12:35:18 +0530

    new fixesss images

--- src/Home/Navbar.jsx
+++ src/Home/Navbar.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
-const Logo1 = "../public/assets/Logo1.png";
+const Logo1 = "../../Public/assets/Logo1.png";
 const ShreeLogo = "../public/assets/shree.jpg";
 const GooglePlay = "../public/assets/googlePlay.png";
 const AppStore = "/assets/appStore.png";
